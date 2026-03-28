02eac05dd7ad147b069251a55ef79b5e17a4acb2
dixonisatoru <dixull99@gmail.com>
2026-03-28 16:49:16 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FoodOrderingSystem.UI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Title = "🔥 Food Ordering System — Dashboard";
+        }
+        catch { }
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Dashboard dashboard = new Dashboard();
+        dashboard.Run();
+    }
+}
